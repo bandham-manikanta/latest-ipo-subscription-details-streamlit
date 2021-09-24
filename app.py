a4384bfaddc24728cb7a28ba5d149abd1887ca4c
Bandham <MBANDHAM@ford.com>
2021-09-25 01:03:02 +0530

    Streamlit

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.write("Hello Streamlit..")
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+st.write(add_selectbox)
